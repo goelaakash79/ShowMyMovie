60b0f846292b5de8dfa8e7ea0dc04c87c3dfb318
aakash <goelaakash79@gmail.com>
2018-01-16 00:06:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("index");    
+});
+
+// INDEX
+app.get("/movies", function(req, res){
+    res.render("index");    
+});
+
+// SEARCH
+app.get("/movies/results", function(req, res) {
+    var movie = req.query.movie;
+    var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + movie;
+    request(url, function(err, response, body){
+        if(!err && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
+        }    
+    });
+});
+
+// SHOW
+app.get("/movies/show/:id", function(req, res) {
+    var movieId = req.params.id;
+    console.log(movieId);
+    var url = "http://www.omdbapi.com/?apikey=thewdb&plot=full&i=" + movieId;
+    request(url, function(err, response, body){
+        if(!err && response.statusCode == 200){
+            var data = JSON.parse(body);
+            console.log(data["Title"]);
+            res.render("show", {data: data});
+        }
+    });   
+});
+
+app.listen("1234", process.env.IP, function(){
+    console.log("Server is running");    
+});
