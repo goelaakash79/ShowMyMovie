38dced937d988a1e7636c85746cc53b3689905c2
goelaakash79 <goelaakash79@gmail.com>
2018-06-18 05:19:04 +0530

    port

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.get("*", function(req, res) {
   res.render("404page");
 });
 
-app.listen("1234", process.env.IP, function() {
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
   console.log("Server is running");
 });
