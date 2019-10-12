a9fc0e33b9e93a85263089902ab1bb0786129093
rishabhmaheshwari7 <rishabh.mah22@gmail.com>
2019-10-12 19:27:08 +0530

    changes

--- app.js
+++ app.js
@@ -86,5 +86,5 @@ app.get("*", function(req, res) {
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
-  console.log("Server is running");
+  console.log("Server is running succesfully");
 });
